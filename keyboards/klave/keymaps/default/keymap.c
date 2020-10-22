16957162165fa8ff5ec74199d9bf68bc94d56518
David Kosorin <david@kosorin.net>
2020-10-22 21:58:22 +0200

    Update Klave keymap

--- keyboards/klave/keymaps/default/keymap.c
+++ keyboards/klave/keymaps/default/keymap.c
@@ -19,9 +19,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [L_BASE] = LAYOUT(
                                                                                                                                                                                                 /**/
     /**/    KC_ESC,     KC_1,       KC_2,       KC_3,       KC_4,       KC_5,                   /**/                KC_6,       KC_7,       KC_8,       KC_9,       KC_0,       KC_MINS,        /**/
-    /**/    K_DIAC,     KC_Q,       KC_W,       KC_K,       KC_P,       KC_G,                   /**/                KC_J,       KC_L,       KC_U,       KC_Y,       KC_SCLN,    K_DIAC,         /**/
-    /**/    K_LSFT,     KC_A,       KC_R,       KC_S,       KC_T,       KC_D,       KC_MUTE,    /**/    K_ENC_SM,   KC_H,       KC_N,       KC_E,       KC_I,       KC_O,       K_RSFT,         /**/
-    /**/    KC_LCTL,    KC_Z,       KC_X,       KC_C,       KC_V,       KC_B,                   /**/                KC_F,       KC_M,       KC_COMM,    KC_DOT,     KC_DQUO,    KC_RCTL,        /**/
+    /**/    K_DIAC,     KC_W,       KC_K,       KC_P,       KC_D,       KC_G,                   /**/                KC_J,       KC_L,       KC_U,       KC_Y,       KC_SCLN,    K_DIAC,         /**/
+    /**/    K_LSFT,     KC_A,       KC_R,       KC_S,       KC_T,       KC_F,       KC_MUTE,    /**/    K_ENC_SM,   KC_M,       KC_N,       KC_E,       KC_I,       KC_O,       K_RSFT,         /**/
+    /**/    KC_LCTL,    KC_Z,       KC_X,       KC_C,       KC_V,       KC_B,                   /**/                KC_Q,       KC_H,       KC_COMM,    KC_DOT,     KC_DQUO,    KC_RCTL,        /**/
     /**/                                                                                                                                                                                        /**/
     /**/                                                                KC_TAB,     KC_LEAD,    /**/    KC_MEH,     KC_DEL,                                                                     /**/
     /**/                            KC_LGUI,    KC_LALT,    K_LWR,      KC_SPC,     KC_ENT,     /**/    KC_HYPR,    KC_BSPC,    K_RSE,      KC_RALT,    KC_RGUI,                                /**/
